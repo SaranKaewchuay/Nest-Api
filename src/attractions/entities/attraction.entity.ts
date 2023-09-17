75309c7b12989070f0e6cd101ec877f678d7fe1f
SaranKaewchuay <saran.ka@mail.wu.ac.th>
2023-09-17 20:52:49 +0700

    first commit

---
+++ src/attractions/entities/attraction.entity.ts
@@ -0,0 +1,32 @@
+
+
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type AttractionDocument = Attraction & Document;
+
+@Schema()
+export class Attraction {
+    @Prop()
+    id: number;
+
+    @Prop()
+    name: string;
+
+    @Prop()
+    detail: string;
+
+    @Prop()
+    coverimage: string;
+
+    @Prop({ type: Number, precision: 10, scale: 8 })
+    latitude: number;
+
+    @Prop({ type: Number, precision: 11, scale: 8 })
+    longitude: number;
+
+
+}
+
+export const AttractionSchema = SchemaFactory.createForClass(Attraction);
